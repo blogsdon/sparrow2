6c8de1f1cbda8785728f8ac5d3c3ab7e9389d818
Benjamin Logsdon <ben.logsdon@sagebase.org>
2017-04-20 07:37:38 -0700

    update aml score pushing function

--- pushAmlSparrow.R
+++ pushAmlSparrow.R
@@ -1,23 +1,48 @@
-synapseClient::synapseLogin()
 system('curl -O http://leelab-data.cs.washington.edu/amlCodes.rda')
+
 load('amlCodes.rda')
+
 geneNames <- rownames(basesFreq)
+
 rownames(basesFreq) <- 1:nrow(basesFreq)
+
 basesFreq <- data.frame(basesFreq,stringsAsFactors=F)
+
 basesFreq$geneName <- geneNames
+
 basesFreq <- dplyr::arrange(basesFreq,desc(sparrow1))
+
 amlSparrowScores <- dplyr::select(basesFreq,geneName,sparrow1)
+
 colnames(amlSparrowScores)[c(2)] <- 'sparrowScore'
 
-
 fileName <- 'amlSparrowScores.csv'
+
 synapseFolderId <- 'syn9650583'
+
 annos <- c('dataType'='analysis',
   'method'='sparrow',
   'disease'='acuteMyeloidLeukemia')
+
 comment <- 'sparrow scores for AML from Logsdon et al. 2015'
-executedVector <-
+
+executedVector<-githubr::getPermlink('blogsdon/sparrow2','pushAmlSparrow.R')
+
 activityName1 <- 'push sparrow scores to synapse'
+
 activityDescription1 <- 'pull sparrow scores from Lee lab server and push to Synapse'
 
-rSynapseUtilities::pushToSynapseWrapper(amlSparrowScores)
+synapseClient::synapseLogin()
+
+write.csv(amlSparrowScores,file=fileName,quote=F)
+
+foo <- synapseClient::File(fileName,
+                           parentId=synapseFolderId,
+                           versionComment=comment)
+
+synapseClient::synSetAnnotations(foo) = as.list(annos)
+
+foo = synapseClient::synStore(foo,
+               executed = as.list(executedVector),
+               activityName = activityName1,
+               activityDescription = activityDescription1)
